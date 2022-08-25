139fd964fc4e80018db64db7f93ee9f79dffa1b4
lxy <892595285@qq.com>
2022-08-25 13:24:16 +0800

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+#include<iterator>
+#include<string>
+#include<algorithm>
+#include<functional>
+#include<utility>
+#include<iostream>
+#include<sstream>
+using namespace std;
+namespace myf{
+template <typename T>
+ostream &print(ostream &os,const T& t)
+{
+    return os<<t;
+}
+
+template <typename T,typename... Args>
+ostream &print(ostream &os,const T& t,const Args&... rest)
+{
+    os<<t<<", ";
+    return print(os,rest...);
+}
+}
+
+
+int main(int argc,char **argv)
+{
+    using namespace myf;
+    print(cout,1,2,3,7);
+    return 0;
+}
